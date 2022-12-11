5665a66be6777d5549a72bb4bbe4f68b7d40d50a
ngyuen bamboo <bamboongyuen@zz.local>
2022-12-11 14:43:48 +0700

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @author bamboongyuen
+ *
+ *         this is my first code!
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World. My name is Bamboo. Nice to see you!");
+	}
+}
